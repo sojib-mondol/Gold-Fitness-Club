d10fc008cb7c8250fffbf51a8f8dd5e21f983279
sojib-sudo <sojibmandol@gmail.com>
2022-09-29 22:20:27 +0600

    some retouch done for bloag

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,11 +5,15 @@ const Blog = () => {
     return (
         <div style={{textAlign: "left"}} className='container shadow-lg p-5 rounded ml-5'>
             <div>
-                <h2>How does react woks?</h2>
+                <h2>1. How does react woks?</h2>
                 <p>React is a JavaScript library developed by Facebook which, among other things, was used to build Instagram.com. Its aim is to allow developers to easily create fast user interfaces for websites and applications alike. The main concept of React. js is virtual DOM.</p>
+                <h6>State Management in React</h6>
+                <p>A state is a JavaScript object that represents a part of a component. It changes whenever a user interacts with the application, rendering a new UI to reflect the modifications.
+
+</p>
             </div>
             <div>
-                <h2 >Difference between state and props?</h2>
+                <h2 >2. Difference between state and props?</h2>
                 <h4 >PROPS</h4>
                 <div >
                 <p>1. The Data is passed from one component to another.</p>
@@ -24,7 +28,7 @@ const Blog = () => {
                 </div>
             </div>
             <div>
-                <h2>use of useeffect in react</h2>
+                <h2>3. Use of useeffect in react</h2>
                 <p>The motivation behind the introduction of useEffect Hook is to eliminate the side-effects of using class-based components. For example, tasks like updating the DOM, fetching data from API end-points, setting up subscriptions or timers, etc can be lead to unwarranted side-effects.</p>
             </div>
         </div>
